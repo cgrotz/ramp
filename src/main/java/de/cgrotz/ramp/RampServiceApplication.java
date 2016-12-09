5183246ced5577bb9ab6b03b8bc46158a46f3c70
Christoph Grotz <christophgrotz@gmail.com>
2016-12-09 17:45:10 +0800

    Initial commit from the atomist bot

---
+++ src/main/java/de/cgrotz/ramp/RampServiceApplication.java
@@ -0,0 +1,12 @@
+package de.cgrotz.ramp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RampServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RampServiceApplication.class, args);
+    }
+}
